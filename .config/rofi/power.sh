2a151271bd6d66861e3407534b9d6e745ae8f86b
sophragoz <sophr.temin@ro.ru>
2025-03-13 22:22:43 +0500

    Add power script and config file rofi

---
+++ .config/rofi/power.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+poweroff=""
+reboot=""
+logout="󰈆"
+
+show_menu() {
+    echo -e "$poweroff\n$logout\n$reboot" | rofi -dmenu -config ~/.config/rofi/power.rasi 
+}
+
+choice=$(show_menu)
+
+case $choice in
+    $poweroff)
+        /usr/sbin/poweroff
+        ;;
+    $logout)
+        bspc quit
+        ;;
+    $reboot)
+        /usr/sbin/reboot 
+        ;;
+    *)
+        echo "Canceled"
+        ;;
+esac
